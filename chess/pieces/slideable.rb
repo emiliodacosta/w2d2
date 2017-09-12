f3ae54b89976ce3cf65945aaebea497150610d32
Emilio da Costa <emidac@gmail.com>
2017-09-12 18:00:34 -0400

    Chess AllMoves Slideable Logic

--- chess/pieces/slideable.rb
+++ chess/pieces/slideable.rb
@@ -20,13 +20,10 @@ module Slideable
 
   def moves
     allmoves=[]
-
     self.move_dirs.each do |dir|
-
       if dir[0] * dir[1] == 0
         n = 0
         until n == 7 || n == -7
-
           if dir[0] == 0
             case dir[1]
             when > 0
@@ -46,17 +43,31 @@ module Slideable
               n -= 1
             end
           end
-
-
         end
-
       else
-        n = 0
-        until n==6
-
-
+        a = 0
+        b = 0
+        until a == 7 || b == -7
+          subarr = []
+          case dir[0]
+          when > 0
+            subarr.concat([dir[0] + a])
+          when < 0
+            subarr.concat([dir[0] - b])
+          end
+          case dir[1]
+          when > 0
+              subarr.concat([dir[1] + a])
+          when < 0
+              subarr.concat([dir[1] - b])
+          end
+          allmoves << subarr
+          a += 1
+          b -= 1
+        end
+      end
+    end
+    allmoves
   end
-
-
 
 end
